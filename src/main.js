9ac68da85dcd46971e694602d2a406ee31fae990
ecang <sandytesar@gmail.com>
2022-05-19 23:25:34 +0700

    all

--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//main.js
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8000/api/'
+
 createApp(App).use(store).use(router).mount('#app')
